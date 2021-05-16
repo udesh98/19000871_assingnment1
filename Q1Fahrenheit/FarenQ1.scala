94691b0258c0b6dabf8fa85460c888a7a5b35275
udesh98 <um4me.98@gmail.com>
2021-05-16 15:52:08 +0530

    Completed all the questions

---
+++ Q1Fahrenheit/FarenQ1.scala
@@ -0,0 +1,5 @@
+object FarenQ1 extends App{
+	def conv(x:Double)=x*1.8 + 32.0;
+
+	println("Temperature in Farenhite is " + conv(35) + " F");
+} 
---
+++ Q2Volume/VolQ2.scala
@@ -0,0 +1,5 @@
+object VolQ2 extends App{
+	def vol(r:Double)=((4.0/3)*(22.0/7)*r*r*r);
+
+	println("Volume of the sphere is " + vol(5));
+} 
